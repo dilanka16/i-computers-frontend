9857229a93d23348ed13edb283e14e3928566788
dilanka16 <savindudilanka16@gmail.com>
2026-01-19 18:54:45 +0530

    tailwincss✏️

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,21 @@ import './App.css'
 function App() {
   
   return (
-    <div className="w-[ 233px] h-[629.75px] border bg-cyan-500">
+    <div className="border w-[600px] h-[600px] bg-gray-400 relative">
+      <div className="w-[500px] h-[500px] bg-yellow-200 flex flex-col items-center justify-center">
+        <div className="w-[100px] h-[100px] bg-blue-600">
+
+        </div>
+         <div className="w-[100px] h-[100px] bg-red-600 fixed left-[550px] top-[550px]">
+
+        </div>
+         <div className="w-[100px] h-[100px] bg-green-600">
+
+        </div>
+         <div className="w-[100px] h-[100px] absolute right-[100px] bottom-[100px] bg-pink-600">
+
+        </div>
+      </div>
 
     </div>
   )
