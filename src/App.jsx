cbb3e302dc70c800bca715341ae2ad7cdf033fb7
dilanka16 <savindudilanka16@gmail.com>
2026-01-20 19:59:20 +0530

    header ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Test from './components/test'
+import HomePage from './pages/homePage'
+import LoginPage from './pages/loginPage'
+import ResgisterPage from './pages/registerPage'
+import AdminPage from './pages/adminPage'
+
 
 
 function App() {
   
   return (
-    <div className="border w-[600px] h-[600px] bg-gray-400 relative">
-      <div className="w-[500px] h-[500px] bg-yellow-200 flex flex-col items-center justify-center">
-        <div className="w-[100px] h-[100px] bg-blue-600">
+    <BrowserRouter>
+    <div className="w-full h-screen bg-primary text-secondary">
+      <Routes path="/">
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/register" element={<ResgisterPage/>} />
+      <Route path="/admin" element={<AdminPage/>} />
 
-        </div>
-         <div className="w-[100px] h-[100px] bg-red-600 fixed left-[550px] top-[550px]">
-
-        </div>
-         <div className="w-[100px] h-[100px] bg-green-600">
-
-        </div>
-         <div className="w-[100px] h-[100px] absolute right-[100px] bottom-[100px] bg-pink-600">
-
-        </div>
-      </div>
+      </Routes>
 
     </div>
+    </BrowserRouter>
   )
 
   
