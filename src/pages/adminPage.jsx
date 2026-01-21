966c376bcfd383a089199471c8c2ccc88c216b21
dilanka16 <savindudilanka16@gmail.com>
2026-01-21 20:32:38 +0530

    admin page✏️

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,5 +1,24 @@
 export default function AdminPage() {
     return(
-        <h1>Admin Page</h1>
+        <div className="w-full h-full flex bg-accent">
+            <div className="w-[300px] bg-accent h-full">
+                <div className="w-full h-[100px] flex items-center text-primary">
+                    <img src="/logo.png" className="h-full"/>
+                    <h1 className="text-2xl">Admin</h1>
+
+                </div>
+                <div className="w-full h-[400px]">
+
+                </div>
+
+            </div>
+            <div className="w-[calc(100%-300px)] h-full max-h-full bg-primary border-[10px] rounded-3xl overflow-y-scroll border-accent">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur optio, iusto molestias at eveniet architecto odio voluptates praesentium, velit, totam quaerat blanditiis ipsa quisquam? Repudiandae, dolorem fuga. Recusandae dolore consequatur consequuntur hic. Unde, neque dolor labore suscipit accusamus quibusdam accusantium perferendis sit modi voluptas veritatis animi incidunt. Quas accusamus eos error eaque sed ex neque animi vero nesciunt sit quis pariatur sapiente dolores assumenda, magni ad tempora, non nostrum nobis aperiam. Deserunt quo, numquam sint consequatur natus ut provident ratione ullam nobis eius animi voluptates, repudiandae voluptatibus earum quidem odio hic unde. Molestiae, animi? Explicabo, minus fuga ut nobis maxime, nisi corporis quae aut quis recusandae in vero nihil non pariatur beatae voluptas! Aperiam blanditiis cumque libero debitis sit similique dolorem illum animi excepturi voluptatum nulla dolor perferendis ipsam alias suscipit odit exercitationem asperiores sint nemo nihil aliquid, aspernatur repellendus, quia nesciunt. Omnis accusantium dolor mollitia rerum nobis non molestias laboriosam, nesciunt similique vel, incidunt eius quas quod excepturi assumenda alias iure sit cumque ipsum ipsa? Molestiae amet quae, dolor voluptatem atque neque nihil asperiores eaque veniam nam unde quisquam debitis perferendis expedita vitae fugit eos delectus alias quam in quia. Labore nobis rem asperiores id inventore commodi optio, architecto ex, doloribus eaque corporis sed. Voluptates excepturi dolores, unde, accusantium beatae ducimus deleniti nihil ipsa facere labore fuga. Recusandae eius culpa omnis non ullam quod animi? Id accusantium at repudiandae vel quam, ad nemo perspiciatis rerum eum maxime amet. Enim autem dolor voluptatibus minima modi esse quis accusantium quaerat, numquam error perferendis placeat est tempore fuga quo in nemo rerum sapiente sed repellat harum. Magni tempore itaque consequuntur nulla explicabo eum impedit doloremque vero amet dolor perferendis aperiam illum corrupti sunt, esse officia maxime incidunt placeat cum iusto. Ullam tempora quod sed nostrum neque fugit adipisci fuga pariatur eligendi perspiciatis excepturi necessitatibus saepe deserunt sunt ex incidunt praesentium porro ipsa, ut maiores atque officia voluptatum dignissimos! A doloribus officia reiciendis veniam eveniet, sint consequatur voluptas temporibus blanditiis unde accusantium quia dicta cupiditate? Sint magni consequuntur eum officiis, eius excepturi eos. Architecto sequi natus odit dolorem? Consectetur saepe esse libero voluptate alias, eius, sint qui id velit omnis sit suscipit impedit odio placeat nostrum numquam! Possimus mollitia autem corrupti nesciunt obcaecati quod cum neque at repudiandae ab quae veniam ex recusandae corporis, sapiente laboriosam dolorem? Eveniet maiores, asperiores in veritatis vero vel dicta dolorem exercitationem neque error. Hic libero blanditiis quae eius rerum quidem, suscipit repudiandae quia modi iste accusantium laudantium et aspernatur facilis deserunt, numquam debitis ipsum est aliquam distinctio molestias assumenda ullam unde. Est aliquam ab quaerat esse veritatis quos sapiente mollitia nobis reprehenderit culpa ducimus iure quae id cum porro, saepe ratione nostrum cupiditate alias soluta? Consectetur id impedit natus itaque hic tempora eum cupiditate blanditiis asperiores eaque quod enim distinctio minus iste numquam rem maxime accusantium, quibusdam ut dolorum. Cum, dolor dolorum fuga similique, quas porro repudiandae culpa iste assumenda, earum voluptatem. Perferendis saepe fugiat incidunt officia numquam blanditiis vero amet aperiam, at, totam accusantium laborum eius?
+
+               
+
+            </div>
+
+        </div>
     )
 }
